2f455ca85f186efa03c7cfd6a0b6a074133b191d
SyedIhsan <syedihsan1998@gmail.com>
2023-08-12 07:26:53 +0800

    Update

--- server.js
+++ server.js
@@ -7,15 +7,22 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 // Your existing server configuration and routes
+// Add any middleware, routes, and static file serving as needed
 
 // Listen for incoming WebSocket connections
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   // Handle updates from clients
-  socket.on('update', (data) => {
-    // Broadcast the update to all connected clients
-    io.emit('update', data);
+  socket.on('newReturn', (returnData) => {
+    // Broadcast the new return data to all connected clients
+    io.emit('returnData', returnData);
+  });
+
+  // Handle delete events from clients
+  socket.on('deleteReturn', (index) => {
+    // Broadcast the delete event to all connected clients
+    io.emit('deleteReturn', index);
   });
 
   // Handle disconnect
@@ -25,7 +32,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
