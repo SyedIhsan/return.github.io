534f1f11e444f264d32f540eaf511bf98951e009
SyedIhsan <syedihsan1998@gmail.com>
2023-08-13 01:27:25 +0800

    Update

--- server.js
+++ server.js
@@ -1,44 +1,58 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// Your existing server configuration and routes
-// Add any middleware, routes, and static file serving as needed
-app.use(express.static(path.join(__dirname, 'return.github.io')));
+// Serve static files (your HTML, CSS, and JS)
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/return.github.io/index.html');
-});
+    res.sendFile(__dirname + '/index.html'); // Adjust the file path if needed
+  });
 
 // Listen for incoming WebSocket connections
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Handle updates from clients
-  socket.on('newReturn', (returnData) => {
-    // Broadcast the new return data to all connected clients
-    io.emit('returnData', returnData);
-  });
+  socket.on("updateStatus", (data) => {
+    io.emit("updatedStatus", {
+        index: data.index,
+        newStatus: data.newStatus,
+        followUpValue: data.followUpValue
+     });
+    });
+    
+    socket.on("updateFollowUp", function (data) {
+        io.emit("updatedFollowUp", data); // Broadcast to all clients
+    });
 
-  // Handle delete events from clients
-  socket.on('deleteReturn', (index) => {
-    // Broadcast the delete event to all connected clients
-    io.emit('deleteReturn', index);
-  });
 
-  // Handle disconnect
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+  // Handle messages from clients
+    socket.on("returnData", (data) => {
+        io.emit("returnData", data);
+    });
+  
+    socket.on("deleteRow", (index) => {
+        // Delete the row with the corresponding index
+        io.emit("deleteRow", index);
+    });
+
+    socket.on("refreshPage", () => {
+        // Refresh the page to synchronize the changes
+        io.emit("refreshPage");
+    });
+
+    // Handle disconnect
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
 });
-
 // Start the server
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
